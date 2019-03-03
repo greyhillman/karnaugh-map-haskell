2e4b697f0f0e61169bfddce1b86e45fdd1b42c88
Grey Hill <infogreytech@gmail.com>
2019-03-02 17:07:06 -0800

    Move to have kmap dynamic

--- frontend/src/Common.hs
+++ frontend/src/Common.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Common where
 
 import Data.Text (Text)
 import qualified Data.Text as T
+
+import qualified Data.Map as Map
+import Data.Maybe (catMaybes)
 
 data Size = Small | Medium | Large
     deriving (Eq, Show, Enum, Bounded)
@@ -12,11 +16,34 @@ showSize Small = "2x2"
 showSize Medium = "2x4"
 showSize Large = "4x4"
 
+data KMap = KMap
+    { kmapSize :: Size
+    , kmapEntries :: Map.Map Text Entry
+    }
 
-data KMap = KMap Size [[Entry]]
+getEntriesForGrid :: KMap -> [Entry]
+getEntriesForGrid (KMap size dict) = catMaybes $ (Map.!?) dict <$> getKeysFromSize size
+
+getKeysFromSize :: Size -> [Text]
+getKeysFromSize Small = [
+    "00", "01",
+    "10", "11"
+    ]
+getKeysFromSize Medium = [
+    "000", "001",
+    "010", "011",
+    "110", "111",
+    "100", "101"
+    ]
+getKeysFromSize Large = [
+    "0000", "0001", "0011", "0010",
+    "0100", "0101", "0111", "0110",
+    "1100", "1101", "1111", "1110",
+    "1000", "1001", "1011", "1010"
+    ]
 
 makeNewKMap :: Size -> KMap
-makeNewKMap Small = KMap Small [[EFalse, EFalse], [EFalse, EFalse]]
+makeNewKMap size = KMap size $ Map.fromList $ (,EFalse) <$> getKeysFromSize size
 
 data Entry = ETrue | EFalse | DontCare
 
--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -9,6 +9,7 @@ import Reflex.Dom
 import Data.Text (Text)
 import qualified Data.Text as T
 import Control.Monad.Fix
+import qualified Data.Map as Map
 
 import Data.FileEmbed
 
@@ -18,15 +19,29 @@ import Common
 main = mainWidgetWithCss ($(embedFile "static/main.css"))
     $ do
         renderHeader
-        el "main" $ do
+        kmap <- el "main" $ mdo
             options <- renderOptions
-            let dKMap = makeNewKMap <$> (optionsSize options)
-            renderMap options dKMap
-        renderFooter
+            dEntries <- renderMap options kmap
+            kmap <- holdKMap options (makeNewKMap Small) dEntries
+            renderMap options kmap
+            return kmap
+        renderFooter kmap
         return ()
 
-renderFooter :: DomBuilder t m => m ()
-renderFooter = el "footer" $ do
+holdKMap :: (Reflex t, MonadHold t m, MonadFix m) => Options t -> KMap -> Dynamic t (Map.Map Text Entry) -> m (Dynamic t KMap)
+holdKMap options initialKMap dEntries = do
+    let eSize = updated $ optionsSize options
+    kmap <- foldDyn ($) initialKMap $ mergeWith (.) $ [
+        const . makeNewKMap <$> eSize
+        ]
+    return kmap
+
+renderFooter :: (PostBuild t m, DomBuilder t m) => Dynamic t KMap -> m ()
+renderFooter dKMap = el "footer" $ do
+    let entries = getEntriesForGrid <$> dKMap
+    let entriesText = (displayEntry <$>) <$> entries
+    el "div" $ do
+        dynText $ T.intercalate " " <$> entriesText
     text "footer"
 
 renderHeader :: DomBuilder t m => m ()
@@ -34,12 +49,12 @@ renderHeader = do
     el "header" $ do
         text "Karanaugh Map Generator"
 
-renderMap :: (MonadSample t m, DomBuilder t m, PostBuild t m, MonadHold t m, MonadFix m) => Options t -> Dynamic t KMap -> m ()
+renderMap :: (MonadSample t m, DomBuilder t m, PostBuild t m, MonadHold t m, MonadFix m) => Options t -> Dynamic t KMap -> m (Dynamic t (Map.Map Text Entry))
 renderMap options dMap = do
     map <- sample $ current dMap
-    showKMap options map
+    return $ constDyn Map.empty 
 
-showKMap options (KMap size entries) = elClass "div" "map" $ do
+showKMap options kmap@(KMap size _) = elClass "div" "map" $ do
     elClass "div" "top" $ text "X"
     elClass "div" "left" $ text "Y"
     elClass "div" "top-num" $ do
@@ -50,9 +65,9 @@ showKMap options (KMap size entries) = elClass "div" "map" $ do
         el "div" $ text "1"
     elClass "div" "grid" $ do
         let dAllowDontCare = optionsAllowDontCare options
-        mapM_ (mapM_  $ mapEntry dAllowDontCare) entries
-
-showEntry entry = elClass "div" "entry" $ text (displayEntry entry)
+        let entries = getEntriesForGrid kmap
+        dEntries <- mapM (changeableMapEntry options) entries
+        return ()
 
 renderEntry :: (DomBuilder t m, PostBuild t m) => Dynamic t Entry -> m (Event t ())
 renderEntry dEntry = do
@@ -60,12 +75,18 @@ renderEntry dEntry = do
         dynText $ displayEntry <$> dEntry
     return $ domEvent Click e
 
-mapEntry :: (DomBuilder t m, PostBuild t m, MonadHold t m, MonadFix m) => Dynamic t Bool -> Entry -> m (Event t Entry)
-mapEntry dAllowDontCare initialEntry = mdo
+changeableMapEntry :: (DomBuilder t m, PostBuild t m, MonadHold t m, MonadFix m) => Options t -> Entry -> m (Dynamic t Entry)
+changeableMapEntry options initialEntry = mdo
     eClick <- renderEntry entry
+    entry <- holdEntry options initialEntry eClick
+    return $ entry
+
+holdEntry :: (Reflex t, MonadHold t m, MonadFix m) => Options t -> Entry -> Event t () -> m (Dynamic t Entry)
+holdEntry options initialEntry eClick = mdo
+    let dAllowDontCare = optionsAllowDontCare options
     let bAllowDontCare = current dAllowDontCare
     let changeEntry = nextEntry <$> bAllowDontCare <@ eClick
-    entry <- foldDyn ($) initialEntry changeEntry
-    return $ updated entry
 
+    entry <- foldDyn ($) initialEntry changeEntry
+    return entry
 
